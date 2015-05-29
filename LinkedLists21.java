dba3a76eb4ca87214dd4626a33df7f61e941568a
Nguyen Tuan Anh <hamsterviel01@gmail.com>
2015-05-29 11:24:37 +0800

    Add new exercises

---
+++ LinkedLists21.java
@@ -0,0 +1,12 @@
+public class LinkedLists21 {
+	public void removeDuplicatedWithBuffer(LinkedListNode node){
+
+	}
+
+	public static void main(String[] args){
+		//how to create a linked list with 6 nodes from SinglyLinkedListNode class??
+		for (int i=0; i<6; i++){
+			SinglyLinkedListNode node = new SinglyLinkedListNode()
+		}
+	}
+}
